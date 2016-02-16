824494f11d1b2dde532ad9c6c875b46c5a1154b8
Jonathan Schapiro <jschapiro@fishsixrc.com>
2016-02-15 18:16:48 -0800

    current workaround

--- server/middleware.js
+++ server/middleware.js
@@ -10,29 +10,11 @@ module.exports = (app) => {
   app.use(bodyParser.json());
   app.use(morgan('dev'));
   app.use('/hooks', ParseCloud.app);
+  
   app.post('/hooks/mosaiek/contribute',(req,res) => {
     console.log('req.body',req.body)
     res.send('hello world');
   });
-  Parse.Cloud.afterSave('MosaicImage', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
-  });
 
 };
 
-//TODO: http://blog.parse.com/learn/using-node-js-with-parse/
-
-
-
-// Parse.Cloud.define('findBacon', (req, res) => {
-//   let token = req.user.getSessionToken();
-//   let query = new Parse.Query('Bacon');
-//   // Pass the session token to the query
-//   query.find({ sessionToken: token }).then((error, result) => {
-//     if (error) {
-//       return res.json(error);
-//     }
-//     res.json(result);
-//   });
-// });
