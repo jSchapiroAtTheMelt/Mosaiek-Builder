c8b821871e891740bd1583c0110a7daec6022f9a
Jonathan Schapiro <jschapiro@fishsixrc.com>
2016-02-15 17:02:09 -0800

    different app.use regex

--- server/middleware.js
+++ server/middleware.js
@@ -4,11 +4,12 @@ let morgan = require('morgan');
 let ParseCloud = require('parse-cloud-express');
 let Parse = ParseCloud.Parse;
 
+
 module.exports = (app) => {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(morgan('dev'));
-  app.use(/\/hooks?/, ParseCloud.app);
+  app.use('/hooks', ParseCloud.app);
 };
 
 //TODO: http://blog.parse.com/learn/using-node-js-with-parse/
