66c0f76dd61f3942f9f9e0191ecc7ba75ef86347
Jonathan Schapiro <jschapiro@fishsixrc.com>
2016-02-15 17:57:49 -0800

    scope change

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ require('dotenv').config({ silent: true });
 
 let app = require('express')();
 
-app.use('/hooks', ParseCloud.app);
-
 require('./server/middleware')(app);
 
 app.listen(process.env.PORT);
